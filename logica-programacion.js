f148ee0898bda57e6eb5114841aaa193dbf67069
CristhianZ2022 <cris12x12@gmail.com>
2025-12-09 23:54:16 -0500

    Add exercise

--- logica-programacion.js
+++ logica-programacion.js
@@ -45,6 +45,7 @@ function anagrama(word1, word2) {
 // console.log(anagrama("car", "car"));
 // console.log(anagrama("car", "cara"));
 
+
 /*
  * Escribe un programa que imprima los 50 primeros números de la sucesión
  * de Fibonacci empezando en 0.
@@ -67,4 +68,72 @@ function fibonacci() {
 // console.log(fibonacci());
 
 
+/*
+ * Escribe un programa que se encargue de comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ */
 
+function primo() {
+  const primos = [];
+  
+  for (let num = 2; num <= 100; num++) {
+    let esPrimo = true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        esPrimo = false;
+        break;
+      }
+    }
+    if (esPrimo) primos.push(num);
+  }
+
+  console.log("Primos del 1 al 100:");
+  console.log(primos.join(", "));
+}
+
+
+/*
+ * Crea una única función (importante que sólo sea una) que sea capaz
+ * de calcular y retornar el área de un polígono.
+ * - La función recibirá por parámetro sólo UN polígono a la vez.
+ * - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+ * - Imprime el cálculo del área de un polígono de cada tipo.
+ */
+
+function area(pol, tipo) { 
+  let area = 0;
+  if (tipo === "Triangulo") {
+    area = (pol.base * pol.altura) / 2;
+    console.log("El área del Triángulo es: " + area);
+  } else if (tipo === "Cuadrado") {
+    area = Math.pow(pol.lado, 2);
+    console.log("El área del Cuadrado es: " + area);
+  } else if (tipo === "Rectángulo") {
+    area = pol.lado * pol.altura;
+    console.log("El área del Rectángulo es: " + area);
+  }
+}
+
+
+/*
+ * Crea un programa que se encargue de calcular el aspect ratio de una
+ * imagen a partir de una url.
+ * - Url de ejemplo:
+ *   https://raw.githubusercontent.com/mouredevmouredev/master/mouredev_github_profile.png
+ * - Por ratio hacemos referencia por ejemplo a los "16:9" de una
+ *   imagen de 1920*1080px.
+ */
+
+async function aspectRatio(url) {
+  const img = new Image();
+  img.src = url;
+
+  await img.decode();
+
+  img.onload = () => {
+    const width = img.width;
+    const height = img.height;
+    const ratio = width / height;
+    console.log(ratio);
+  };
+}
